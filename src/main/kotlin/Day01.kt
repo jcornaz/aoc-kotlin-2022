7ecc274b9ca2ae67639e2d971aeb775cf9313048
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-12-01 21:51:08 +0100

    refactor day 1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,17 +1,17 @@
 object Day01 {
     fun part1(input: String): Long =
-        input.splitToSequence("\n\n")
-            .map { elf ->
-                elf.lineSequence().sumOf { it.toLong() }
-            }
+        input.elfSequence()
             .max()
 
     fun part2(input: String): Long =
-        input.splitToSequence("\n\n")
-            .map { elf ->
-                elf.lineSequence().sumOf { it.toLong() }
-            }
+        input.elfSequence()
             .sortedDescending()
             .take(3)
             .sum()
+
+    private fun String.elfSequence() =
+        splitToSequence("\n\n")
+            .map { elf ->
+                elf.lineSequence().sumOf { it.toLong() }
+            }
 }
