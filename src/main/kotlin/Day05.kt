3f4e2dee13591c741a2f2237114734c140bd4e64
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-12-05 17:06:50 +0100

    day 5 - part 1

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,7 +1,34 @@
-object Day05 {
+import java.util.*
 
-    @Suppress("UNUSED_PARAMETER")
-    fun part1(input: String): Long = TODO()
+object Day05 {
+    // move 1 from 2 to 1
+    fun part1(input: String): String {
+        val stacks = parseInitialState(input)
+        input.lines().dropWhile { it.isNotBlank() }.drop(1).forEach { instruction ->
+            val (n, _, from, _, to) = instruction.removePrefix("move ").split(" ")
+            repeat(n.toInt()) {
+                stacks[to.toInt() - 1].push(stacks[from.toInt() - 1].pop())
+            }
+        }
+
+        return stacks
+            .map { it.pop() }
+            .joinToString(separator = "")
+    }
+
+    private fun parseInitialState(input: String): List<Stack<Char>> {
+        val indices = generateSequence(1) { it + 4 }
+        val initialStateLines = input.lines().takeWhile { "1" !in it }
+        return indices
+            .takeWhile { it < initialStateLines.first().length }
+            .map { i ->
+                val stack = Stack<Char>()
+                initialStateLines.mapNotNull { line -> line.getOrNull(i)?.takeIf { !it.isWhitespace() } }
+                    .forEach(stack::push)
+                stack.reverse()
+                stack
+            }.toList()
+    }
 
     @Suppress("UNUSED_PARAMETER")
     fun part2(input: String): Long = TODO()
--- src/test/kotlin/Day05Test.kt
+++ src/test/kotlin/Day05Test.kt
@@ -2,28 +2,79 @@ import io.kotest.core.spec.style.FunSpec
 import org.amshove.kluent.shouldBeEqualTo
 
 private val EXAMPLE = """
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
 """.trimIndent()
 
 private val INPUT = Day05Test::class.java.getResource("/day05_input.txt")?.readText().orEmpty().trim()
 
 class Day05Test : FunSpec({
     context("part 1") {
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
-        xtest("should return expected output for the example") {
-            Day05.part1(EXAMPLE) shouldBeEqualTo 0
-        }
-
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
-        xtest("should return expected output for the puzzle input") {
-            Day05.part1(INPUT) shouldBeEqualTo 0
-        }
-
-        listOf<Pair<String, Long>>(
-            // TODO Add more test cases here
+        listOf(
+            """
+                [N]
+                 1 
+            """.trimIndent() to "N",
+            """
+                [C]
+                [N]
+                 1 
+            """.trimIndent() to "C",
+            """
+                [C] [A]
+                 1   2
+            """.trimIndent() to "CA",
+            """
+                [B]    
+                [C] [A]
+                 1   2
+            """.trimIndent() to "BA",
+            """
+               |     [B]
+               | [C] [A]
+               |  1   2
+            """.trimMargin("| ") to "CB",
+            """
+               |     [D]    
+               | [N] [C]    
+               | [Z] [M] [P]
+               |  1   2   3 
+            """.trimMargin("| ") to "NDP",
+            """
+               |     [D]     
+               | [N] [C]    
+               | [Z] [M] [P]
+               |  1   2   3
+               | 
+               | move 1 from 2 to 1
+            """.trimMargin("| ") to "DCP",
+            """
+               |     [D]     
+               | [N] [C]    
+               | [Z] [M] [P]
+               |  1   2   3
+               | 
+               | move 2 from 2 to 3
+            """.trimMargin("| ") to "NMC",
         ).forEach { (input, expectedOutput) ->
-            test("part1(\"${input}\") = $expectedOutput") {
+            test("topOf(\"${input}\") = $expectedOutput") {
                 Day05.part1(input) shouldBeEqualTo expectedOutput
             }
+        }
+
+        test("should return expected output for the example") {
+            Day05.part1(EXAMPLE) shouldBeEqualTo "CMZ"
+        }
+
+        test("should return expected output for the puzzle input") {
+            Day05.part1(INPUT) shouldBeEqualTo "TGWSMRBPN"
         }
     }
 
