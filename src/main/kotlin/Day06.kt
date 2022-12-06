b92b5d006ccd79b17a87cddb911878517767868c
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-12-06 16:51:47 +0100

    minor refactoring of day 6

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,15 +1,12 @@
 object Day06 {
 
-    @Suppress("UNUSED_PARAMETER")
     fun part1(input: String) = findSignalPosition(input, 4)
-
-
-    @Suppress("UNUSED_PARAMETER")
     fun part2(input: String): Int = findSignalPosition(input, 14)
 
     private fun findSignalPosition(input: String, n: Int) = input.windowedSequence(n)
         .withIndex()
-        .firstOrNull() { (_, v) ->
+        .firstOrNull { (_, v) ->
             v.toSet().size == v.length
-        }?.let { it.index + n } ?: -1
+        }
+        ?.let { it.index + n } ?: -1
 }
