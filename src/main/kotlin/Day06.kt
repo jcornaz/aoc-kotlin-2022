e9b07f5e1ee08beb0925a92772a09869979d9cca
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-12-06 16:26:29 +0100

    day 6 - part 1

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,7 +1,12 @@
 object Day06 {
 
     @Suppress("UNUSED_PARAMETER")
-    fun part1(input: String): Long = TODO()
+    fun part1(input: String) =
+        input.windowedSequence(4)
+            .withIndex()
+            .first { (_, v) ->
+                v.toSet().size == v.length
+            }.index + 4
 
     @Suppress("UNUSED_PARAMETER")
     fun part2(input: String): Long = TODO()
--- src/test/kotlin/Day06Test.kt
+++ src/test/kotlin/Day06Test.kt
@@ -1,25 +1,19 @@
 import io.kotest.core.spec.style.FunSpec
 import org.amshove.kluent.shouldBeEqualTo
 
-private val EXAMPLE = """
-""".trimIndent()
-
 private val INPUT = Day06Test::class.java.getResource("/day06_input.txt")?.readText().orEmpty().trim()
 
 class Day06Test : FunSpec({
     context("part 1") {
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
-        xtest("should return expected output for the example") {
-            Day06.part1(EXAMPLE) shouldBeEqualTo 0
-        }
-
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
-        xtest("should return expected output for the puzzle input") {
+        test("should return expected output for the puzzle input") {
             Day06.part1(INPUT) shouldBeEqualTo 0
         }
 
-        listOf<Pair<String, Long>>(
-            // TODO Add more test cases here
+        listOf(
+            "abcd" to 4,
+            "abcde" to 4,
+            "aabcde" to 5,
+            "mjqjpqmgbljsphdztnvjfqwrcgsmlb" to 7,
         ).forEach { (input, expectedOutput) ->
             test("part1(\"${input}\") = $expectedOutput") {
                 Day06.part1(input) shouldBeEqualTo expectedOutput
