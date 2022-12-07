3a9346e01132fad18127f249a6fa3941efdeac0e
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-12-07 17:30:40 +0100

    day 7

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,9 +1,41 @@
+import java.io.File
+import java.util.StringJoiner
+
+private const val DISK_SPACE = 70000000
+private const val NEEDED_SPACE = 30000000
+
 object Day07 {
 
-    @Suppress("UNUSED_PARAMETER")
-    fun part1(input: String): Long = TODO()
+    fun part1(input: String): Int {
+        val directories = scanDirectory(input)
+        return directories.values.filter { it <= 100_000 }.sum()
+    }
 
-    @Suppress("UNUSED_PARAMETER")
-    fun part2(input: String): Long = TODO()
+    fun part2(input: String): Int {
+        val directories = scanDirectory(input)
+        val used = directories[listOf(" /")] ?: 0
+        val needToFree = NEEDED_SPACE - (DISK_SPACE - used)
+        return directories.values.sorted().dropWhile { it < needToFree  }.first()
+    }
+
+    private fun scanDirectory(input: String): MutableMap<List<String>, Int> {
+        val currentPath = mutableListOf<String>()
+        val directories = mutableMapOf<List<String>, Int>()
+        input.lineSequence()
+            .forEach { line ->
+                if (line == "\$ cd ..") {
+                    currentPath.removeLast()
+                } else if (line.startsWith("\$ cd")) {
+                    currentPath += line.removePrefix("\$ cd")
+                } else if (line.matches(Regex("^\\d+.*"))) {
+                    val size = line.split(" ").first().toInt()
+                    currentPath.indices.forEach { i ->
+                        val dirPath = (0..i).map { currentPath[it] }
+                        directories[dirPath] = (directories[dirPath] ?: 0) + size
+                    }
+                }
+            }
+        return directories
+    }
 
 }
--- src/test/kotlin/Day07Test.kt
+++ src/test/kotlin/Day07Test.kt
@@ -2,48 +2,51 @@ import io.kotest.core.spec.style.FunSpec
 import org.amshove.kluent.shouldBeEqualTo
 
 private val EXAMPLE = """
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
 """.trimIndent()
 
 private val INPUT = Day07Test::class.java.getResource("/day07_input.txt")?.readText().orEmpty().trim()
 
 class Day07Test : FunSpec({
     context("part 1") {
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
-        xtest("should return expected output for the example") {
-            Day07.part1(EXAMPLE) shouldBeEqualTo 0
+        test("should return expected output for the example") {
+            Day07.part1(EXAMPLE) shouldBeEqualTo 95437
         }
 
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
-        xtest("should return expected output for the puzzle input") {
-            Day07.part1(INPUT) shouldBeEqualTo 0
-        }
-
-        listOf<Pair<String, Long>>(
-            // TODO Add more test cases here
-        ).forEach { (input, expectedOutput) ->
-            test("part1(\"${input}\") = $expectedOutput") {
-                Day07.part1(input) shouldBeEqualTo expectedOutput
-            }
+        test("should return expected output for the puzzle input") {
+            Day07.part1(INPUT) shouldBeEqualTo 1453349
         }
     }
 
     context("part 2") {
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
-        xtest("should return expected output for the example") {
-            Day07.part2(EXAMPLE) shouldBeEqualTo 0
+        test("should return expected output for the example") {
+            Day07.part2(EXAMPLE) shouldBeEqualTo 24933642
         }
 
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
-        xtest("should return expected output for the puzzle input") {
-            Day07.part2(INPUT) shouldBeEqualTo 0
-        }
-
-        listOf<Pair<String, Long>>(
-            // TODO Add more test cases here
-        ).forEach { (input, expectedOutput) ->
-            test("part2(\"${input}\") = $expectedOutput") {
-                Day07.part2(input) shouldBeEqualTo expectedOutput
-            }
+        test("should return expected output for the puzzle input") {
+            Day07.part2(INPUT) shouldBeEqualTo 2948823
         }
     }
 })
