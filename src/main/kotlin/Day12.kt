b96f6c2f811491fe4f79dd8bdf671f89a89e652c
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-12-12 17:22:26 +0100

    start day 12

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -6,4 +6,70 @@ object Day12 {
     @Suppress("UNUSED_PARAMETER")
     fun part2(input: String): Long = TODO()
 
+    private const val START_CHARACTER = 'S'
+    private const val GOAL_CHARACTER = 'E'
+
+    fun findPath(input: String): String {
+        val map = parseMap(input)
+        var currentState = findInitialState(map)
+
+        val maxY = map.size - 1
+        val maxX = map.first().size - 1
+
+        val outputMap: List<MutableList<Char>> = map.map { it.mapTo(mutableListOf()) { '.' } }
+
+        while (map[currentState.y][currentState.x] != GOAL_CHARACTER) {
+            print(map[currentState.y][currentState.x])
+            val (direction, nextState) = getSuccessors(currentState, maxX, maxY)
+//                .filter { (_, coordinate) ->
+//                    map[coordinate.y][coordinate.x] == GOAL_CHARACTER || map[coordinate.y][coordinate.x] - map[currentState.y][currentState.x] <= 1
+//                }
+                .maxBy { (_, coordinate) ->
+                    val value = map[coordinate.y][coordinate.x]
+                    if (value == GOAL_CHARACTER) 'z' + 1 else value
+                }
+            outputMap[currentState.y][currentState.x] = direction.char
+            currentState = nextState
+        }
+
+        outputMap[currentState.y][currentState.x] = GOAL_CHARACTER
+
+        return outputMap.joinToString(separator = "\n") { it.joinToString(separator = "")}
+    }
+
+    private fun parseMap(input: String): List<List<Char>> = input.lines().map { it.toList() }
+
+    private fun findInitialState(map: List<List<Char>>) : Coordinate =
+        map.withIndex()
+            .map { (row, l) ->
+                Coordinate(l.withIndex().find { (_, c) -> c == START_CHARACTER }!!.index, row)
+            }
+            .first()
+
+    private fun getSuccessors(currentCoordinate: Coordinate, maxX: Int, maxY: Int) = Direction.values()
+        .map { it to it.getNextSquare(currentCoordinate) }
+        .filter { (_, target) -> target.x in (0 .. maxX) && target.y in (0 .. maxY) }
+
+    class Coordinate(val x: Int, val y: Int)
+
+    enum class Direction(val char: Char) {
+        UP('^') {
+            override fun getNextSquare(currentCoordinate: Coordinate) =
+                Coordinate(currentCoordinate.x, currentCoordinate.y - 1)
+        },
+        DOWN('v') {
+            override fun getNextSquare(currentCoordinate: Coordinate) =
+                Coordinate(currentCoordinate.x, currentCoordinate.y + 1)
+        },
+        LEFT('<') {
+            override fun getNextSquare(currentCoordinate: Coordinate): Coordinate =
+                Coordinate(currentCoordinate.x - 1, currentCoordinate.y)
+        },
+        RIGHT('>') {
+            override fun getNextSquare(currentCoordinate: Coordinate) =
+                Coordinate(currentCoordinate.x + 1, currentCoordinate.y)
+        };
+        
+        abstract fun getNextSquare(currentCoordinate: Coordinate) : Coordinate
+    }
 }
--- src/test/kotlin/Day12Test.kt
+++ src/test/kotlin/Day12Test.kt
@@ -2,6 +2,19 @@ import io.kotest.core.spec.style.FunSpec
 import org.amshove.kluent.shouldBeEqualTo
 
 private val EXAMPLE = """
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+""".trimIndent()
+
+private val EXAMPLE_PATH =  """
+v..v<<<<
+>v.vv<<^
+.>vv>E^^
+..v>>>^^
+..>>>>>^
 """.trimIndent()
 
 private val INPUT = Day12Test::class.java.getResource("/day12_input.txt")?.readText().orEmpty().trim()
@@ -10,7 +23,7 @@ class Day12Test : FunSpec({
     context("part 1") {
         // TODO Set the expected value and enable the test by removing the 'x' prefix
         xtest("should return expected output for the example") {
-            Day12.part1(EXAMPLE) shouldBeEqualTo 0
+            Day12.part1(EXAMPLE) shouldBeEqualTo 31
         }
 
         // TODO Set the expected value and enable the test by removing the 'x' prefix
@@ -18,11 +31,16 @@ class Day12Test : FunSpec({
             Day12.part1(INPUT) shouldBeEqualTo 0
         }
 
-        listOf<Pair<String, Long>>(
-            // TODO Add more test cases here
+        xtest("map direction for example") {
+            Day12.findPath(EXAMPLE) shouldBeEqualTo EXAMPLE_PATH
+        }
+
+        listOf<Pair<String, String>>(
+            "SbcdefghijklmnopqrstuvwxyE" to ">>>>>>>>>>>>>>>>>>>>>>>>>E",
+            "SbcdefghijklmnopqrstuvwxyE".reversed() to "E<<<<<<<<<<<<<<<<<<<<<<<<<",
         ).forEach { (input, expectedOutput) ->
             test("part1(\"${input}\") = $expectedOutput") {
-                Day12.part1(input) shouldBeEqualTo expectedOutput
+                Day12.findPath(input) shouldBeEqualTo expectedOutput
             }
         }
     }
