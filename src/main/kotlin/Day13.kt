d85fc04b9b424c95ccc647c8e4268cf936007216
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-12-13 17:11:22 +0100

    start day 13

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -1,9 +1,29 @@
 object Day13 {
 
-    @Suppress("UNUSED_PARAMETER")
-    fun part1(input: String): Long = TODO()
+    fun part1(input: String): Int =
+        input.trim().split("\n\n").map { it.trim() }.withIndex()
+            .filter { (_, both) ->
+                val r = isInCorrectOrder(both.lines().first() to both.lines().last())
+                println("$both -> $r")
+                r
+            }
+            .sumOf { it.index + 1 }
 
     @Suppress("UNUSED_PARAMETER")
     fun part2(input: String): Long = TODO()
 
+    fun isInCorrectOrder(pair: Pair<String, String>): Boolean {
+        val (left, right) = pair
+        return if (left.startsWith("[") && right.startsWith("[")) {
+            val l = left.removePrefix("[").removeSuffix("]").split(",").filter { it.isNotBlank() }
+            val r = right.removePrefix("[").removeSuffix("]").split(",").filter { it.isNotBlank() }
+            l.zip(r).all { (a, b) -> isInCorrectOrder(a to b) } && l.size <= r.size
+        } else if (left.startsWith("[")){
+            isInCorrectOrder(left to "[$right]")
+        } else if (right.startsWith("[")) {
+            isInCorrectOrder("[$left]" to right)
+        } else {
+            left.toInt() <= right.toInt()
+        }
+    }
 }
--- src/test/kotlin/Day13Test.kt
+++ src/test/kotlin/Day13Test.kt
@@ -2,27 +2,60 @@ import io.kotest.core.spec.style.FunSpec
 import org.amshove.kluent.shouldBeEqualTo
 
 private val EXAMPLE = """
+    [1,1,3,1,1]
+    [1,1,5,1,1]
+
+    [[1],[2,3,4]]
+    [[1],4]
+
+    [9]
+    [[8,7,6]]
+
+    [[4,4],4,4]
+    [[4,4],4,4,4]
+
+    [7,7,7,7]
+    [7,7,7]
+
+    []
+    [3]
+
+    [[[]]]
+    [[]]
+
+    [1,[2,[3,[4,[5,6,7]]]],8,9]
+    [1,[2,[3,[4,[5,6,0]]]],8,9]
 """.trimIndent()
 
 private val INPUT = Day13Test::class.java.getResource("/day13_input.txt")?.readText().orEmpty().trim()
 
 class Day13Test : FunSpec({
     context("part 1") {
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
         xtest("should return expected output for the example") {
-            Day13.part1(EXAMPLE) shouldBeEqualTo 0
+            Day13.part1(EXAMPLE) shouldBeEqualTo 13
         }
 
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
         xtest("should return expected output for the puzzle input") {
             Day13.part1(INPUT) shouldBeEqualTo 0
         }
 
-        listOf<Pair<String, Long>>(
-            // TODO Add more test cases here
+        listOf(
+            ("[1]" to "[2]") to true,
+            ("[2]" to "[1]") to false,
+            ("[1,2]" to "[2,3]") to true,
+            ("[1]" to "[2,3]") to true,
+            ("[1,2]" to "[2]") to false,
+            ("[3,2]" to "[2]") to false,
+            ("[1]" to "[[2]]") to true,
+            ("[[1]]" to "[2]") to true,
+            ("[]" to "[3]") to true,
+            ("[[[]]]" to "[[]]") to false,
+            ("[]" to "[[]]") to true,
+            ("[1,1,3,1,1]" to "[1,1,5,1,1]") to true,
+            // ("[[4,4],4,4]" to "[[4,4],4,4,4]") to true,
         ).forEach { (input, expectedOutput) ->
-            test("part1(\"${input}\") = $expectedOutput") {
-                Day13.part1(input) shouldBeEqualTo expectedOutput
+            test("isInRightOrder($input) = $expectedOutput") {
+                Day13.isInCorrectOrder(input) shouldBeEqualTo expectedOutput
             }
         }
     }
