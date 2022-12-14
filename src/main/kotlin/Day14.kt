40e571063b122b77a00f36a83020e1ba541f3c9d
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-12-14 17:42:40 +0100

    start day 14

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,9 +1,75 @@
 object Day14 {
 
-    @Suppress("UNUSED_PARAMETER")
-    fun part1(input: String): Long = TODO()
+    fun part1(input: String): Long {
+        println(input)
+        val cave = Cave.buildCave(input)
+        var counter = 0L
+        while (cave.dropSand() != null) counter++
+        return counter
+        return counter
+    }
 
     @Suppress("UNUSED_PARAMETER")
     fun part2(input: String): Long = TODO()
 
+
+    // val cave: Cave = Day14.buildCave("2,0")
+
+    class Cave private constructor(private val blockedSpaces : MutableSet<Position>) {
+
+        private val max = blockedSpaces.maxBy { it.y }.y
+
+        fun dropSand(): Position? {
+            println(blockedSpaces)
+            return generateSequence(Position(500, 0)) { it.getNextPosition() }
+                .takeWhile { it.y <= max }
+                .last()
+                .takeIf { it.y < max }
+                ?.also { blockedSpaces.add(it) }
+        }
+
+
+        private fun Position.getNextPosition(): Position? {
+            if (!isBlocked(x, y + 1)) return Position(x, y + 1)
+            if (!isBlocked(x - 1, y + 1)) return Position(x - 1, y + 1)
+            if (!isBlocked(x + 1, y + 1)) return Position(x + 1, y + 1)
+            return null
+        }
+
+        fun isBlocked(x: Int, y: Int): Boolean = Position(x, y) in blockedSpaces
+
+        companion object {
+
+            fun buildCave(input: String): Cave {
+                val set = mutableSetOf<Position>()
+                input.lines().forEach { line ->
+                    line.split(" -> ")
+                        .map {
+                            val (x, y) = it.split(",")
+                            Position(x.toInt(), y.toInt())
+                        }
+                        .fold<Position, Position?>(null) { prev, next ->
+                            if (prev == null) {
+                                set.add(next)
+                                next
+                            } else {
+                                if (prev.x != next.x) {
+                                    for (x in minOf( prev.x, next.x)..maxOf( prev.x, next.x)) {
+                                        set.add(Position(x, next.y))
+                                    }
+                                } else {
+                                    for (y in minOf( prev.y, next.y)..maxOf( prev.y, next.y)) {
+                                        set.add(Position(next.x, y))
+                                    }
+                                }
+                                prev
+                            }
+                        }
+                }
+                return Cave(set)
+            }
+        }
+    }
+
+    data class Position(val x: Int, val y: Int)
 }
--- src/test/kotlin/Day14Test.kt
+++ src/test/kotlin/Day14Test.kt
@@ -1,16 +1,19 @@
 import io.kotest.core.spec.style.FunSpec
-import org.amshove.kluent.shouldBeEqualTo
+import Day14.Cave
+import org.amshove.kluent.*
+import kotlin.test.assertFails
 
 private val EXAMPLE = """
+498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9
 """.trimIndent()
 
 private val INPUT = Day14Test::class.java.getResource("/day14_input.txt")?.readText().orEmpty().trim()
 
 class Day14Test : FunSpec({
     context("part 1") {
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
         xtest("should return expected output for the example") {
-            Day14.part1(EXAMPLE) shouldBeEqualTo 0
+            Day14.part1(EXAMPLE) shouldBeEqualTo 24
         }
 
         // TODO Set the expected value and enable the test by removing the 'x' prefix
@@ -18,11 +21,87 @@ class Day14Test : FunSpec({
             Day14.part1(INPUT) shouldBeEqualTo 0
         }
 
-        listOf<Pair<String, Long>>(
-            // TODO Add more test cases here
-        ).forEach { (input, expectedOutput) ->
-            test("part1(\"${input}\") = $expectedOutput") {
-                Day14.part1(input) shouldBeEqualTo expectedOutput
+        test("should detect obstacle") {
+            val cave: Cave = Cave.buildCave("2,0")
+            cave.isBlocked(2, 0).shouldBeTrue()
+        }
+
+        test("should detect empty space cave") {
+            val cave: Cave = Cave.buildCave("2,0")
+            cave.isBlocked(3,0).shouldBeFalse()
+        }
+
+        test("should detect obstacles in a line") {
+            val cave: Cave = Cave.buildCave("2,0 -> 4,0")
+            cave.isBlocked(2, 0).shouldBeTrue()
+            cave.isBlocked(3, 0).shouldBeTrue()
+            cave.isBlocked(4, 0).shouldBeTrue()
+        }
+
+        test("should detect obstacles in a reverse line ") {
+            val cave: Cave = Cave.buildCave("2,0 -> 0,0")
+            cave.isBlocked(2, 0).shouldBeTrue()
+            cave.isBlocked(1, 0).shouldBeTrue()
+            cave.isBlocked(0, 0).shouldBeTrue()
+        }
+
+        test("should detect obstacles in a vertical line") {
+            val cave: Cave = Cave.buildCave("2,0 -> 2,2")
+            cave.isBlocked(2, 0).shouldBeTrue()
+            cave.isBlocked(2, 1).shouldBeTrue()
+            cave.isBlocked(2, 2).shouldBeTrue()
+        }
+
+        test("should detect obstacles in a reversed vertical line") {
+            val cave: Cave = Cave.buildCave("2,2 -> 2,0")
+            cave.isBlocked(2, 0).shouldBeTrue()
+            cave.isBlocked(2, 1).shouldBeTrue()
+            cave.isBlocked(2, 2).shouldBeTrue()
+        }
+
+        test("should detect obstacles in rock formation with direction change") {
+            val cave: Cave = Cave.buildCave("2,0 -> 2,2 -> 4,2")
+            cave.isBlocked(2, 0).shouldBeTrue()
+            cave.isBlocked(2, 1).shouldBeTrue()
+            cave.isBlocked(2, 2).shouldBeTrue()
+            cave.isBlocked(3, 2).shouldBeTrue()
+            cave.isBlocked(4, 2).shouldBeTrue()
+        }
+
+        test("should detect obstacles in two rock formations") {
+            val cave: Cave = Cave.buildCave("2,0 -> 2,2\n12,0 -> 12,2")
+            cave.isBlocked(2, 0).shouldBeTrue()
+            cave.isBlocked(2, 1).shouldBeTrue()
+            cave.isBlocked(2, 2).shouldBeTrue()
+            cave.isBlocked(2, 3).shouldBeFalse()
+            cave.isBlocked(12, 0).shouldBeTrue()
+            cave.isBlocked(12, 1).shouldBeTrue()
+            cave.isBlocked(12, 2).shouldBeTrue()
+        }
+
+        listOf(
+            Triple("400,1 -> 600,1", 1, listOf(500 to 0)),
+            Triple("400,2 -> 600,2", 1, listOf(500 to 1)),
+            Triple("400,2 -> 600,2", 2, listOf(500 to 1, 499 to 1)),
+            Triple("400,2 -> 600,2", 3, listOf(500 to 1, 499 to 1, 501 to 1)),
+        ).forEach { (input, n, expectedBlocked) ->
+            test("sand simulation for $input and $n steps without overflow") {
+                val cave = Cave.buildCave(input)
+                repeat(n) { cave.dropSand().shouldNotBeNull() }
+                expectedBlocked.forEach { (x, y) ->
+                    cave.isBlocked(x,y).shouldBeTrue()
+                }
+            }
+        }
+
+        test("sand simulation detects overflow") {
+            val cave = Cave.buildCave("1,1")
+            cave.dropSand().shouldBeNull()
+        }
+
+        xtest("should fail if the input for the cave is wrong") {
+            assertFails {
+                Cave.buildCave("2,0 -> 3,2")
             }
         }
     }
