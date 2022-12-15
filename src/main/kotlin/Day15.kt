db4777da71684f848ba258bcdc11a0c5fa71f59a
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-12-15 16:58:31 +0100

    day 15 - part 1

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -1,9 +1,44 @@
+import kotlin.math.abs
+
 object Day15 {
 
-    @Suppress("UNUSED_PARAMETER")
-    fun part1(input: String): Long = TODO()
+    fun part1(input: String): Long = numberOfNonBeacon(input, 2000000).toLong()
 
     @Suppress("UNUSED_PARAMETER")
     fun part2(input: String): Long = TODO()
+
+    private val LINE_REGEX = Regex("Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)")
+
+    fun numberOfNonBeacon(input: String, y: Int): Int {
+        val beacons = mutableSetOf<Position>()
+        val sensors = input.lines()
+            .map {
+                val (sensorPosition, beaconPosition) = parseLine(it)
+                beacons.add(beaconPosition)
+                Sensor(sensorPosition, sensorPosition.distanceTo(beaconPosition))
+            }
+        val minX = sensors.minOf { it.minX }
+        val maxX = sensors.maxOf { it.maxX }
+        return (minX..maxX)
+            .map { Position(it, y) }
+            .count { it !in beacons && sensors.any { sensor -> sensor.isWithinRange(it) } }
+    }
+
+    private fun parseLine(line: String): Pair<Position, Position> {
+        val (_, sx, sy, bx, by) = LINE_REGEX.find(line)!!.groupValues
+        return Position(sx.toInt(), sy.toInt()) to Position(bx.toInt(), by.toInt())
+    }
+
+    data class Position(val x: Int, val y: Int) {
+        fun distanceTo(other: Position): Int {
+            return abs(x - other.x) + abs(y - other.y)
+        }
+    }
+
+    data class Sensor(val position: Position, val range: Int) {
+        val minX = position.x - range
+        val maxX = position.x + range
+        fun isWithinRange(otherPosition: Position) = position.distanceTo(otherPosition) <= range
+    }
 
 }
--- src/test/kotlin/Day15Test.kt
+++ src/test/kotlin/Day15Test.kt
@@ -2,27 +2,49 @@ import io.kotest.core.spec.style.FunSpec
 import org.amshove.kluent.shouldBeEqualTo
 
 private val EXAMPLE = """
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
 """.trimIndent()
 
 private val INPUT = Day15Test::class.java.getResource("/day15_input.txt")?.readText().orEmpty().trim()
 
 class Day15Test : FunSpec({
     context("part 1") {
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
-        xtest("should return expected output for the example") {
-            Day15.part1(EXAMPLE) shouldBeEqualTo 0
+        test("should return expected output for the example") {
+            Day15.numberOfNonBeacon(EXAMPLE, 10) shouldBeEqualTo 26
         }
 
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
-        xtest("should return expected output for the puzzle input") {
-            Day15.part1(INPUT) shouldBeEqualTo 0
+        test("should return expected output for the puzzle input") {
+            Day15.part1(INPUT) shouldBeEqualTo 4560025
         }
 
-        listOf<Pair<String, Long>>(
-            // TODO Add more test cases here
-        ).forEach { (input, expectedOutput) ->
-            test("part1(\"${input}\") = $expectedOutput") {
-                Day15.part1(input) shouldBeEqualTo expectedOutput
+        ///
+        ///    S   SB
+        ///   ###  #
+        ///    B
+        listOf(
+            ("Sensor at x=0, y=0: closest beacon is at x=2, y=0" to 0) to 4,
+            ("Sensor at x=0, y=0: closest beacon is at x=-2, y=0" to 0) to 4,
+            ("Sensor at x=0, y=0: closest beacon is at x=0, y=2" to 1) to 3,
+            ("""
+                Sensor at x=0, y=0: closest beacon is at x=0, y=2
+                Sensor at x=4, y=0: closest beacon is at x=5, y=0
+            """.trimIndent() to 1) to 4,
+        ).forEach { (case, expectedOutput) ->
+            test("part1(\"${case}\") = $expectedOutput") {
+                Day15.numberOfNonBeacon(case.first, case.second) shouldBeEqualTo expectedOutput
             }
         }
     }
