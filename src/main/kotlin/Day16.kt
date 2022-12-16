21bd38faa0243a27a980ad6545bdffb95124cb5a
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-12-16 17:04:13 +0100

    draft day 16

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -1,9 +1,56 @@
+import java.util.*
+
 object Day16 {
 
-    @Suppress("UNUSED_PARAMETER")
-    fun part1(input: String): Long = TODO()
+    fun part1(input: String): Long {
+        TODO()
+    }
 
     @Suppress("UNUSED_PARAMETER")
     fun part2(input: String): Long = TODO()
 
+    data class Valve(val id: String, val rate: Int)
+
+    data class SearchState(val valve: Valve, val openValves: Map<Valve, Boolean>)
+
+    sealed class Action {
+        object OpenValve : Action()
+        data class GotoValve(val name: String): Action()
+    }
+
+    class Solver(private val initialState: SearchState, private val connections: Map<Valve, Collection<Valve>>) {
+
+        private val visitedStates: MutableSet<SearchState> = mutableSetOf()
+
+        private val priorityQueue: PriorityQueue<Pair<SearchState, Int>> = PriorityQueue<Pair<SearchState, Int>>(compareBy { -it.second })
+            .apply { add(initialState to 0) }
+
+        private fun getSuccessorState(searchState: SearchState, score: Int, step: Int): Sequence<Pair<SearchState, Int>> {
+            val connectedStates = connections[searchState.valve]?.asSequence().orEmpty().map {
+                SearchState(it, searchState.openValves) to score
+            }
+
+            return if(searchState.openValves[searchState.valve] == false && searchState.valve.rate != 0)
+                connectedStates + (SearchState(searchState.valve, searchState.openValves + (searchState.valve to true)) to computeScore(score, step, searchState.valve.rate))
+            else
+                connectedStates
+        }
+
+        private fun computeScore(score: Int, step: Int, rate: Int) = score + ((30 - step) * rate)
+
+        fun solve() : Int {
+            var bestStateThusFar = initialState to 0
+            var step = 0
+
+            while (!priorityQueue.isEmpty() && step <= 30) {
+                step++
+                val (state, score) = priorityQueue.poll()
+                if (bestStateThusFar.second < score) bestStateThusFar = state to score
+                getSuccessorState(state, score, step)
+                    .forEach { priorityQueue.add(it) }
+            }
+
+            return bestStateThusFar.second
+        }
+    }
 }
--- src/test/kotlin/Day16Test.kt
+++ src/test/kotlin/Day16Test.kt
@@ -2,15 +2,24 @@ import io.kotest.core.spec.style.FunSpec
 import org.amshove.kluent.shouldBeEqualTo
 
 private val EXAMPLE = """
+Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II    
 """.trimIndent()
 
 private val INPUT = Day16Test::class.java.getResource("/day16_input.txt")?.readText().orEmpty().trim()
 
 class Day16Test : FunSpec({
     context("part 1") {
-        // TODO Set the expected value and enable the test by removing the 'x' prefix
         xtest("should return expected output for the example") {
-            Day16.part1(EXAMPLE) shouldBeEqualTo 0
+            Day16.part1(EXAMPLE) shouldBeEqualTo 1651
         }
 
         // TODO Set the expected value and enable the test by removing the 'x' prefix
